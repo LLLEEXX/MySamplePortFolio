eae8f2fcc6cb2d31eb1bdebe30e901f6683aec4a
leafarsinom <96124912+LLLEEXX@users.noreply.github.com>
2023-11-08 15:44:06 +0800

    another Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/MySamplePortFolio/",
   plugins: [react()],
 });
 
